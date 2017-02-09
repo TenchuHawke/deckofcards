34e199d149cf78ad4a47aff9550bcbfcdaf4c2cc
TenchuHawke <hawke@unrestrainedgames.com>
2017-02-08 16:30:15 -0800

    Started work on Texas Hold Em

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 using System;
+using System.Collections.Generic;
 
-namespace CardGame
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace CardGame {
+    class Program {
+        static void Main (string [] args) {
+            Deck pokerDeck = new Deck ();
+            List<Player> players = new List<Player> ();
+            players.Add (new Player ("Jeff"));
+            players.Add (new Player ("Bill"));
+            players.Add (new Player ("Bob"));
+            players.Add (new Player ("Sam"));
+            // Beginning of hand
+            //first deal
+            public void deal (Deck deckCurrent, List playersList) 
+            pokerDeck.shuffle ();
+            foreach (Player gamer in players) {
+                gamer.draw(pokerDeck);
+            }
+
         }
     }
 }
